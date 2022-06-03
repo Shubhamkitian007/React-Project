30ff599d8376c307cb1c3d6f21c29eac9825fb4c
shubhamkitian007 <imshubhamkitan@gmail.com>
2022-06-03 12:16:29 +0530

    React Project

---
+++ src/compo/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./index.css"
+const Footer = () => {
+  return (
+        <>
+            <div className='footer' >
+                <p>&#169; 2022 Shubham Kumar </p>
+            </div>
+        </>
+    )
+}
+
+export default Footer;
